6fe257e69dce70365950a4f26e202af3eb9a17ff
Kostas Tsalikidis <tsalikidhs@gmail.com>
2023-03-05 14:05:03 +0200

    socket initialization refactor

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const {
 } = require('uuid');
 const Server = require('./server');
 
-let db = new sqlite3.Database('./d83c1db25e5ff24f29560bf6b0305310.db', (err) => {
+let db = new sqlite3.Database('./database.db', (err) => {
   if (err) {
     console.error(err.message);
   } else {
-    console.log('Connected to the chinook database.');
+    console.log('Connected to the database.');
 
     const server = new Server();
     const io = server.start();
@@ -25,57 +25,167 @@ process.on('exit', function() {
 
 function initIo(io) {
   io.on('connection', (socket) => {
-    socket.on('login', (data) => {
-      console.log('login', data);
+    socket.on('login', async (data) => {
       if (data.key === null) {
-        const newKey = md5(uuidv1());
-        const balance = 100000;
-        db.run(`INSERT INTO accounts (username, balance, key) VALUES (?, ?, ?)`, ['Guest', balance, newKey], function(err) {
-          if (err) {
-            console.log(err.message);
-          } else {
-            socket.emit('login', {
-              status: 'logged-in',
-              key: newKey,
-              balance: balance,
-            });
-          }
-        });
-      } else {
-        db.all(`SELECT * FROM accounts WHERE key = ?`, [data.key], function(err, rows) {
-          if (err) {
-            console.log(err.message);
-          } else {
-            if (rows.length === 1) {
-              socket.emit('login', {
-                status: 'logged-in',
-                key: data.key,
-                balance: rows[0].balance,
-              });
+        // a new user is trying to login
+        const key = md5(uuidv1());
+        const username = 'Guest';
+        const balance = 10000.00;
+        try {
+          await createNewUser(username, balance, key);
 
-              db.run(`UPDATE accounts SET last_login = ? WHERE id = ?`, [(new Date()).getTime(), rows[0].id]);
-            } else {
-              // invalid login key
-            }
-          }
-        });
+          socket.emit('login', {
+            status: 'logged-in',
+            key,
+            username,
+            balance,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      } else if (data.key) {
+        // a user is trying to login with local key
+        try {
+          const user = await getUser(data.key);
+
+          socket.emit('login', {
+            status: 'logged-in',
+            key: data.key,
+            username: user.username,
+            balance: user.balance,
+          });
+        } catch (err) {
+          console.log(err);
+        }
       }
     });
 
-    socket.on('gamestate', (data) => {
-      db.all(`SELECT * FROM accounts WHERE key = ?`, [data.key], function(err, rows) {
-        if (err) {
-          console.log(err.message);
-        } else {
-          if (rows.length === 1) {
-            socket.emit('gamestate', {
-              balance: rows[0].balance,
-            });
-          } else {
-            // invalid key
-          }
-        }
-      });
+    socket.on('gamestate', async (data) => {
+      try {
+        const account = await getUser(data.key);
+        const gamestate = await getOrCreateGamestate(account.id, data.gameId);
+
+        socket.emit('gamestate', {
+          balance: account.balance,
+          bet: gamestate.bet,
+          coinValue: gamestate.coin_value,
+          reels: JSON.parse(gamestate.reels),
+        });
+      } catch (err) {
+        console.log(err);
+      }
     });
   });
+}
+
+function generateDefaultReelsPosition(gameId) {
+  const position = [];
+
+  switch (gameId) {
+    case 'rock-climber':
+      const reelsCount = 5;
+      const reelPositions = 3;
+      const symbolsCount = 8;
+      for (let i = 0; i < reelsCount; i++) {
+        position.push(Array.from(Array(reelPositions + 1)).map(() => {
+          return parseInt(Math.random() * symbolsCount) + 1;
+        }));
+      }
+      break;
+  }
+
+  return position;
+}
+
+function createNewUser(username, balance, key) {
+  let resolveFn;
+  let rejectFn;
+  const creationPromise = new Promise((resolve, reject) => {
+    resolveFn = resolve;
+    rejectFn = reject;
+  });
+
+  db.run(`INSERT INTO accounts (username, balance, key) VALUES (?, ?, ?)`, [username, balance, key], function(err) {
+    if (err) {
+      rejectFn(err.message);
+    } else {
+      resolveFn(this.lastID);
+    }
+  });
+
+  return creationPromise;
+}
+
+function getUser(key) {
+  let resolveFn;
+  let rejectFn;
+  const getUserPromise = new Promise((resolve, reject) => {
+    resolveFn = resolve;
+    rejectFn = reject;
+  });
+
+  db.all(`SELECT * FROM accounts WHERE key = ?`, [key], function(err, rows) {
+    if (err) {
+      rejectFn(err.message);
+    } else {
+      if (rows.length === 1) {
+        db.run(`UPDATE accounts SET last_login = ? WHERE id = ?`, [(new Date()).getTime(), rows[0].id], function(err) {
+          if (err) {
+            rejectFn(err.message);
+          } else {
+            resolveFn(rows[0]);
+          }
+        });
+      } else {
+        rejectFn('invalid login key');
+      }
+    }
+  });
+
+  return getUserPromise;
+}
+
+function getOrCreateGamestate(userId, gameId) {
+  let resolveFn;
+  let rejectFn;
+  const getGamestatePromise = new Promise((resolve, reject) => {
+    resolveFn = resolve;
+    rejectFn = reject;
+  });
+
+  db.all(`SELECT * FROM gamestates WHERE user_id = ? AND game_id = ?`, [userId, gameId], async function(err, rows) {
+    if (err) {
+      rejectFn(err.message);
+    } else {
+      if (rows.length === 1) {
+        // retrieve gamestate
+        resolveFn(rows[0]);
+      } else {
+        // create new gamestate
+        const bet = 10;
+        const coinValue = 0.10;
+        const reels = JSON.stringify(generateDefaultReelsPosition(gameId));
+        
+        const newGamestate = await new Promise((resolve) => {
+          db.run(`INSERT INTO gamestates (user_id, game_id, reels, bet, coin_value) VALUES (?, ?, ?, ?, ?)`, [
+            userId,
+            gameId,
+            reels,
+            bet,
+            coinValue,
+          ], function(err) {
+            if (err) {
+              rejectFn(err.message);
+            } else {
+              resolve({ reels, bet, coin_value: coinValue });
+            }
+          });
+        });
+
+        resolveFn(newGamestate);
+      }
+    }
+  });
+
+  return getGamestatePromise;
 }
